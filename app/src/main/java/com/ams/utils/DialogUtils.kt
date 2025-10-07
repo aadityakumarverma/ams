cf1ffd4388ca59e81c4670d3cf9c6e39e7fa3036
aadityakumarverma <raushankv217@gmail.com>
2025-10-07 12:49:36 +0530

    feat(VerifyOtp): fragment created

---
+++ app/src/main/java/com/ams/utils/DialogUtils.kt
@@ -0,0 +1,12 @@
+package com.ams.utils
+
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+
+object DialogUtils {
+
+
+}
